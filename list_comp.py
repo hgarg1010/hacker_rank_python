74cd382bfd0006447ec8b9e4c6e180a14cb0a911
Harshit Garg <hgarg1010@gmail.com>
2020-11-08 18:39:07 +0530

    Added Comments in 2 files

--- list_comp.py
+++ list_comp.py
@@ -1,3 +1,4 @@
+# Adding comments
 x = 2
 y = 2
 z = 2
--- lists.py
+++ lists.py
@@ -1,4 +1,4 @@
-
+# adding comments
 list_container = []
 
 n = int(input())
@@ -6,7 +6,7 @@ for i in range(n):
     data = input().split(" ")
     command_len = len(data)
     command_name = data[0]
-    
+
     if command_len == 1:
         # print command
         if command_name == "print":
@@ -16,18 +16,17 @@ for i in range(n):
         elif command_name == "reverse":
             list_container = list_container[::-1]
         else:
-            #pop
+            # pop
             list_container.pop()
-
 
     elif command_len == 2:
         # remove append
         if command_name == "remove":
             list_container.remove(int(data[1]))
         else:
-            #append
+            # append
             list_container.append(int(data[1]))
 
     elif command_len == 3:
         # insert
-        list_container.insert(int(data[1]),int(data[2]))
+        list_container.insert(int(data[1]), int(data[2]))
